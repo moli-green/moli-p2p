d13d5d9cbd0510eed19abda318de1dea8dcb8fe0
moli <moli_green@runbox.com>
2026-02-05 22:06:56 +0900

    revert: Remove LibreJS compliance attempts (abandoned)

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -24,8 +24,6 @@ export default defineConfig({
                 entryFileNames: 'assets/[name].js',
                 chunkFileNames: 'assets/[name].js',
                 assetFileNames: 'assets/[name].[ext]',
-                banner: '// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0',
-                footer: '// @license-end'
             }
         },
         minify: 'terser',
